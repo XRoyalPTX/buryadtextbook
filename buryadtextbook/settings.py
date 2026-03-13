c142a34613a09f1df81450bbe9127d0873fb5884
Архинчеев Владислав Петрович <arkhincheevvp@student.bmstu.ru>
2026-03-13 19:06:41 +0300

    1 импорт bootstrap 2 импорт шрифта Arsenal (поддерживает бурятский) 3 минимальная верстка base.html

--- buryadtextbook/settings.py
+++ buryadtextbook/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -56,7 +55,9 @@ ROOT_URLCONF = 'buryadtextbook.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
