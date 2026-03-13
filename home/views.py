8a1603fc9f5069f8db26c7fc6e5c976351021702
Архинчеев Владислав Петрович <arkhincheevvp@student.bmstu.ru>
2026-03-13 17:54:06 +0300

    1 Добавлен шаблонный html файл base.html и таблица стилей к нему base.css

--- home/views.py
+++ home/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, "home/homepage.html", context=None)
+    time = timezone.now()
+    return render(request, "home/homepage.html", context={
+        "time": time,
+    })
